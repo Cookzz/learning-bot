5d1f719572c2f3a4d4cd04f28c0103068394bf69
Cookzz <souyuuki12@gmail.com>
2018-07-26 22:47:33 +0800

    More updates

--- learningbot.js
+++ learningbot.js
@@ -9,6 +9,9 @@ const token = 'NDcwNTkxOTY5MTExMDQ4MTkz.DjYjXw.lmXMckefdHp8IztNLZZFmEjk1V0';
 
 const ytdl = require('ytdl-core');
 
+const neuralnetwork = require('./neuralnetwork.js');
+const network = new neuralnetwork();
+
 //custom emotes
 const bobrage = "<:bobrage:464489494570074112>";
 const estus = "<:estus:470297656854446081>";
@@ -29,6 +32,11 @@ client.on('message', msg => {
   if (msg.member.id === '394146948686413825'){
     msg.channel.send("I am the better bob!");
     msg.channel.send(bobrage);
+  }
+
+  else if (msg.member.id === '231820363212914689'){
+    const discordMessage = msg.content.trim().split(/ +/g);
+    network.bagOfWords(discordMessage);
   }
 });
 
